a566d9fc1ba2c864bad3ade474aebc95715c8b75
Janelle Tam <janelletam.work@gmail.com>
2024-01-27 14:18:26 -0500

    Add connection between server and client

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-
+from flask_cors import CORS
+    
+# app instance
 app = Flask(__name__)
-app.secret_key = "ActionLink"
+CORS(app)
 
-@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home():
-    return render_template('index.html')
+    return {"members": ["Member1", "Member2", "Member3"]}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
